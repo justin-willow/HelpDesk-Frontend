7da68c6b0753729cffa25677e40e19059e9d2b43
Justin Willow <justinjwillow@gmail.com>
2023-11-29 12:06:12 -0800

    ticket list and details html

--- src/app/components/ticket-details/ticket-details.component.ts
+++ src/app/components/ticket-details/ticket-details.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { TicketService } from 'src/app/services/ticket.service';
 
 @Component({
   selector: 'app-ticket-details',
   templateUrl: './ticket-details.component.html',
-  styleUrls: ['./ticket-details.component.css']
+  styleUrls: ['./ticket-details.component.css'],
 })
-export class TicketDetailsComponent {
+export class TicketDetailsComponent implements OnInit {
+  ticketId!: number;
+  ticket: any;
 
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private ticketService: TicketService
+  ) {}
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe((params) => {
+      this.ticketId = +params.get('id')!;
+      this.ticketService.getTicketById(this.ticketId).subscribe((ticket) => {
+        this.ticket = ticket;
+      });
+    });
+  }
+
+  goBack(): void {
+    this.router.navigate(['/ticket-list']);
+  }
+
+  goToResolveTicket(): void {
+    if (this.ticket) {
+      this.router.navigate(['/resolve-ticket', this.ticket.id]);
+    } else {
+      console.error('Ticket is undefined. Cannot navigate to resolve-ticket.');
+    }
+  }
 }
--- src/app/components/ticket-list/ticket-list.component.ts
+++ src/app/components/ticket-list/ticket-list.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TicketService } from 'src/app/services/ticket.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-ticket-list',
   templateUrl: './ticket-list.component.html',
-  styleUrls: ['./ticket-list.component.css']
+  styleUrls: ['./ticket-list.component.css'],
 })
-export class TicketListComponent {
+export class TicketListComponent implements OnInit {
+  tickets: any[] = [];
+  filteredTickets: any[] = [];
+  showFavorites: boolean = false;
+  userId: number = 0;
 
+  constructor(private ticketService: TicketService, private router: Router) {}
+
+  ngOnInit(): void {
+    this.loadTickets();
+  }
+
+  loadTickets(): void {
+    this.ticketService.getTickets().subscribe((tickets) => {
+      this.tickets = tickets;
+      this.applyFilter();
+    });
+  }
+
+  applyFilter(): void {
+    this.filteredTickets = this.showFavorites
+      ? this.tickets.filter((ticket) => ticket.isFavorite)
+      : this.tickets;
+  }
 }
